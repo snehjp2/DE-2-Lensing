41cca532566cc585563632fa145fc1f7aebe1a38
snehjp2 <snehjp2@gmail.com>
2023-05-07 15:36:13 -0400

    new ddp

--- src/scripts/train_ddp.py
+++ src/scripts/train_ddp.py
@@ -13,19 +13,10 @@ from autoencoder import load_autoencoder
 from dataset import LensingDataset
 from tqdm import tqdm
 import random
-
-import argparse
-import os
-import sys
-import tempfile
-from urllib.parse import urlparse
-
-import torch
+from socket import gethostname
 import torch.distributed as dist
-import torch.nn as nn
-import torch.optim as optim
-
 from torch.nn.parallel import DistributedDataParallel as DDP
+
 
 def set_all_seeds(num):
     random.seed(num)
@@ -37,103 +28,59 @@ def set_all_seeds(num):
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
-def train_model(model, train_dataloader, val_dataloader, optimizer, scheduler = None, epochs=100, device='cuda', save_dir='checkpoints', early_stopping_patience=10, report_interval=5):
+def train(model, train_dataloader, optimizer, epoch, device='cuda', save_dir='checkpoints'):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    model.to(device)
-    model = DDP(model)
-    
-    print("Model Loaded to Device!")
-    best_val_loss = 0
-    no_improvement_count = 0
-    train_losses, val_losses, steps = [], [], []
+    train_losses, steps = [], [], []
     print("Training Started!")
-    for epoch in range(epochs):
-        model.train()
-        train_loss = 0.0
-        for i, batch in tqdm(enumerate(train_dataloader, 0), unit="batch", total=len(train_dataloader)):
+
+    train_loss = 0.0
+    model.train()
+    for i, batch in tqdm(enumerate(train_dataloader, 0), unit="batch", total=len(train_dataloader)):
+        original_image, lensed_image, label, params = batch
+        original_image, lensed_image, params = original_image.to(device), lensed_image.to(device), params.to(device)
+        optimizer.zero_grad()
+        img_output, param_output = model(lensed_image)
+        loss = nn.MSELoss()
+        img_loss = loss(img_output, lensed_image)
+        param_loss = loss(param_output, params)
+        sum_loss = img_loss + param_loss
+        sum_loss.backward()
+        optimizer.step()
+        train_loss += sum_loss.item()
+        train_losses.append(sum_loss.item())
+
+    train_loss /= len(train_dataloader)
+    print(f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4e}")
+    return train_loss
+
+
+def validate(model, val_dataloader, device='cuda'):
+    model.eval()
+    val_loss = 0.0
+    val_losses = []
+    with torch.no_grad():
+        for batch in val_dataloader:
             original_image, lensed_image, label, params = batch
             original_image, lensed_image, params = original_image.to(device), lensed_image.to(device), params.to(device)
-            
-            optimizer.zero_grad()
             img_output, param_output = model(lensed_image)
             loss = nn.MSELoss()
             img_loss = loss(img_output, lensed_image)
             param_loss = loss(param_output, params)
             sum_loss = img_loss + param_loss
-            sum_loss.backward()
-            optimizer.step()
+            val_loss += sum_loss.item()
+            val_losses.append(sum_loss.item())
 
-            train_loss += sum_loss.item()
-            train_losses.append(sum_loss.item())
-            steps.append(epoch * len(train_dataloader) + i + 1)
+    val_loss /= len(val_dataloader)
+    return val_loss
 
-        train_loss /= len(train_dataloader)
-        print(f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4e}")
 
-        if scheduler is not None:
-            scheduler.step()
-
-        if (epoch + 1) % report_interval == 0:
-            model.eval()
-            val_loss = 0.0
-
-            with torch.no_grad():
-                for batch in val_dataloader:
-                    original_image, lensed_image, label, params = batch
-                    original_image, lensed_image, params = original_image.to(device), lensed_image.to(device), params.to(device)
-                    img_output, param_output = model(lensed_image)
-                    loss = nn.MSELoss()
-                    img_loss = loss(img_output, lensed_image)
-                    param_loss = loss(param_output, params)
-                    loss = img_loss + param_loss
-                    sum_loss = img_loss + param_loss
-                    val_loss += sum_loss.item()
-                    val_losses.append(sum_loss.item())
-
-            val_loss /= len(val_dataloader)
-            lr = scheduler.get_last_lr()[0] if scheduler is not None else optimizer.param_groups[0]['lr']
-            print(f"Epoch: {epoch + 1}, Validation Loss: {val_loss:.4f}, Learning rate: {lr}")
-
-            if val_loss > best_val_loss:
-                best_val_loss= val_loss
-                no_improvement_count = 0
-                best_val_epoch = epoch + 1
-                torch.save(model.module.state_dict(), os.path.join(save_dir, f"best_model.pt"))
-            else:
-                no_improvement_count += 1
-
-            if no_improvement_count >= early_stopping_patience:
-                print(f"Early stopping after {early_stopping_patience} epochs without improvement.")
-                break
-
-    torch.save(model.module.state_dict(), os.path.join(save_dir, "final_model.pt"))
+def setup(rank, world_size):
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
     
-    # Plot loss vs. training step graph
-    plt.figure(figsize=(10, 5))
-    plt.plot(steps, train_losses)
-    plt.xlabel('Training Steps')
-    plt.ylabel('Loss')
-    plt.title('Train Loss vs. Training Steps')
-    plt.savefig(os.path.join(save_dir, "train_loss_vs_training_steps.png"), bbox_inches='tight')
-    
-    plt.figure(figsize=(10, 5))
-    plt.plot(steps, val_losses)
-    plt.xlabel('Training Steps')
-    plt.ylabel('Validation Loss')
-    plt.title('Validation Loss vs. Training Steps')
-    plt.savefig(os.path.join(save_dir, "val_loss_vs_training_steps.png"), bbox_inches='tight')
-    
-    return best_val_epoch, best_val_loss, train_losses[-1]
-
 def main(config):
-    model = load_autoencoder()
-    params_to_optimize = [p for p in model.parameters() if p.requires_grad]
-    optimizer = optim.AdamW(params_to_optimize, lr = config['parameters']['lr'], 
-                            weight_decay = config['parameters']['weight_decay'])
-
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones = config['parameters']['milestones'],gamma=config['parameters']['lr_decay'])
     
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -145,6 +92,8 @@ def main(config):
         transforms.RandomVerticalFlip(p=0.3),
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    save_dir = config['save_dir'] + 'autoencoder_' + timestr
     
     print("Loading train dataset!")
     start = time.time()
@@ -152,42 +101,91 @@ def main(config):
     end = time.time()
     print(f"dataset loaded in {end - start:.3f} s")
     
-        # These are the parameters used to initialize the process group
-    env_dict = {
-        key: os.environ[key]
-        for key in ("MASTER_ADDR", "MASTER_PORT", "RANK", "WORLD_SIZE")
-    }
-    
-    print(f"[{os.getpid()}] Initializing process group with: {env_dict}")  
-    dist.init_process_group(backend="nccl")
-
-    print(
-        f"[{os.getpid()}]: world_size = {dist.get_world_size()}, "
-        + f"rank = {dist.get_rank()}, backend={dist.get_backend()} \n", end=''
-    )
-    
     val_len = int(config['parameters']['val_size'] * len(train_dataset))
     train_len = len(train_dataset) - val_len
     train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_len, val_len])
-    train_dataloader = DataLoader(train_dataset, batch_size=config['parameters']['batch_size'], shuffle=True, pin_memory=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=config['parameters']['batch_size'], shuffle=True, pin_memory=True)
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    save_dir = config['save_dir'] + config['model'] + '_' + timestr
+
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["SLURM_PROCID"])
+    gpus_per_node = int(os.environ["SLURM_GPUS_ON_NODE"])
+    assert gpus_per_node == torch.cuda.device_count()
+    print(f"Hello from rank {rank} of {world_size} on {gethostname()} where there are" \
+          f" {gpus_per_node} allocated GPUs per node.", flush=True)
+    setup(rank, world_size)
+    if rank == 0: print(f"Group initialized? {dist.is_initialized()}", flush=True)
+    local_rank = rank - gpus_per_node * (rank // gpus_per_node)
+    torch.cuda.set_device(local_rank)
+    print(f"host: {gethostname()}, rank: {rank}, local_rank: {local_rank}")
     
-    best_val_epoch, best_val_acc, final_loss = train_model(model, train_dataloader, val_dataloader, optimizer, scheduler, 
-                                                           epochs=config['parameters']['epochs'], device=device, 
-                                                           save_dir=save_dir,early_stopping_patience=config['parameters']['early_stopping'], 
-                                                           report_interval=config['parameters']['report_interval'])
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
+    train_dataloader = DataLoader(train_dataset, batch_size=config['parameters']['batch_size'], sampler = train_sampler,
+                                  shuffle=True, num_workers=int(os.environ["SLURM_CPUS_PER_TASK"]), pin_memory=True)                
+    val_dataloader = DataLoader(val_dataset, batch_size=config['parameters']['batch_size'], shuffle=True, 
+                                num_workers=int(os.environ["SLURM_CPUS_PER_TASK"]), pin_memory=True)
     
+    model = load_autoencoder().to(local_rank)
+    ddp_model = DDP(model, device_ids=[local_rank])
+    optimizer = optim.AdamW(ddp_model.parameters(), lr = config['parameters']['lr'], 
+                            weight_decay = config['parameters']['weight_decay'])
+
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones = config['parameters']['milestones'], gamma=config['parameters']['lr_decay'])
+    
+    train_losses = []
+    val_losses = []
+    best_val_loss = float('inf')
+    best_val_epoch = 0
+    final_train_loss = None
+    early_stopping_patience = config['parameters']['early_stopping_patience']
+    early_stopping_counter = 0
+    
+    for epoch in range(1, epochs=config['parameters']['epochs'] + 1):
+        train_loss = train(model = ddp_model, device=local_rank, train_loader=train_dataloader, optimizer=optimizer, epoch=epoch)
+        
+        if epoch % config['parameters']['report_interval'] == 0:
+            if rank == 0:
+                val_loss = validate(model=ddp_model, val_dataloader=val_dataloader, device=local_rank)
+
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    best_val_epoch = epoch
+                    if rank == 0:
+                        torch.save(model.state_dict(), os.path.join(save_dir, f"best_model.pt"))
+                        
+                    early_stopping_counter = 0
+                    
+                else:
+                    early_stopping_counter += 1
+                    
+                    if early_stopping_counter >= early_stopping_patience:
+                        print("Early stopping triggered. No improvement in validation loss.")
+                        break
+                    
+        scheduler.step()
+        
+        if epoch == config['parameters']['epochs']:
+            final_train_loss = train_loss  # Capture the final training loss at the last epoch
+            if rank == 0:
+                torch.save(model.state_dict(), os.path.join(save_dir, "final_model.pt"))
+
+    if rank == 0:
+        
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(1, config['parameters']['epochs'] + 1), train_losses, label='Train Loss')
+        plt.plot(range(config['parameters']['report_interval'], config['parameters']['epochs'] + 1, config['parameters']['report_interval']),
+                 val_losses, label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss')
+        plt.legend()
+        plt.savefig(os.path.join(save_dir, 'loss_curve.png'), bbox_inches='tight')
+        plt.close()
+
     dist.destroy_process_group()
     
-    print('Training Done')
-    
-    config['best_val_acc'] = best_val_acc
+    config['best_val_loss'] = best_val_loss
     config['best_val_epoch'] = best_val_epoch
-    config['final_loss'] = final_loss
-    # config['feature_fields'] = feature_fields
+    config['final_train_loss'] = final_train_loss
 
     file = open(f'{save_dir}/config.yaml',"w")
     yaml.dump(config, file)
@@ -205,7 +203,6 @@ if __name__ == '__main__':
                     help='Location of the config file')
 
     args = parser.parse_args()
-
 
     with open(args.config, 'r') as f:
         config = yaml.safe_load(f)
