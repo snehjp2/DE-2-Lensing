cc85b022c1c4aeb126e1dcd99cd33a356d521d70
purvikpatel <purvikpatel619@gmail.com>
2023-05-13 20:11:50 -0400

    Added pytorch lightning training

--- src/scripts/autoencoder.py
+++ src/scripts/autoencoder.py
@@ -4,16 +4,19 @@ from torch.nn import functional as F
 from e2cnn import gspaces
 from e2cnn import nn as e2cnn_nn
 import torchvision
+import lightning.pytorch as pl
+import argparse
+import yaml
+# encoder_fields = [96, 48, 24]
 
-# encoder_fields = [96, 48, 24]   
 
-class Encoder(torch.nn.Module):
-    def __init__(self, N, encoder_fields = [48, 24, 16], reflections = False):
-        super(Encoder, self).__init__()
-        
+class EquivariantEncoder(torch.nn.Module):
+    def __init__(self, N, encoder_fields=[48, 24, 16], reflections=False):
+        super(EquivariantEncoder, self).__init__()
+
         self.N = N
         self.encoder_fields = encoder_fields
-        
+
         if (reflections == True) and (self.N == 1):
             self.r2_act = gspaces.Flip2dOnR2()
 
@@ -22,51 +25,59 @@ class Encoder(torch.nn.Module):
 
         else:
             self.r2_act = gspaces.Rot2dOnR2(N=self.N)
-            
-        in_type = e2cnn_nn.FieldType(self.r2_act, 3*[self.r2_act.trivial_repr]) ## 3 channels
-        
+
+        in_type = e2cnn_nn.FieldType(
+            self.r2_act, 3*[self.r2_act.trivial_repr])  # 3 channels
+
         self.input_type = in_type
-        
-        out_type = e2cnn_nn.FieldType(self.r2_act, self.encoder_fields[0]*[self.r2_act.regular_repr])
-        
+
+        out_type = e2cnn_nn.FieldType(
+            self.r2_act, self.encoder_fields[0]*[self.r2_act.regular_repr])
+
         self.block1 = e2cnn_nn.SequentialModule(
             e2cnn_nn.MaskModule(in_type, 256, margin=1),
-            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False, stride=1),
+            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5,
+                            padding=2, bias=False, stride=1),
             e2cnn_nn.InnerBatchNorm(out_type),
             e2cnn_nn.ReLU(out_type, inplace=True)
         )
-        
-        self.pool1 = e2cnn_nn.SequentialModule(
-            e2cnn_nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
-        )
-        
-        in_type = self.block1.out_type
-        
-        out_type = e2cnn_nn.FieldType(self.r2_act, self.encoder_fields[1]*[self.r2_act.regular_repr])
-        
-        self.block2 = e2cnn_nn.SequentialModule(
-            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False, stride=2),
-            e2cnn_nn.InnerBatchNorm(out_type),
-            e2cnn_nn.ReLU(out_type, inplace=True)
-        )
-        
-        in_type = self.block2.out_type
-        out_type = e2cnn_nn.FieldType(self.r2_act, self.encoder_fields[2]*[self.r2_act.regular_repr])
 
-        
-        self.block3 = e2cnn_nn.SequentialModule(
-            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False, stride=2),
+        self.pool1 = e2cnn_nn.SequentialModule(
+            e2cnn_nn.PointwiseAvgPoolAntialiased(
+                out_type, sigma=0.66, stride=2)
+        )
+
+        in_type = self.block1.out_type
+
+        out_type = e2cnn_nn.FieldType(
+            self.r2_act, self.encoder_fields[1]*[self.r2_act.regular_repr])
+
+        self.block2 = e2cnn_nn.SequentialModule(
+            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5,
+                            padding=2, bias=False, stride=2),
             e2cnn_nn.InnerBatchNorm(out_type),
             e2cnn_nn.ReLU(out_type, inplace=True)
         )
-        
-        self.pool2 = e2cnn_nn.SequentialModule(
-            e2cnn_nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+
+        in_type = self.block2.out_type
+        out_type = e2cnn_nn.FieldType(
+            self.r2_act, self.encoder_fields[2]*[self.r2_act.regular_repr])
+
+        self.block3 = e2cnn_nn.SequentialModule(
+            e2cnn_nn.R2Conv(in_type, out_type, kernel_size=5,
+                            padding=2, bias=False, stride=2),
+            e2cnn_nn.InnerBatchNorm(out_type),
+            e2cnn_nn.ReLU(out_type, inplace=True)
         )
-        
+
+        self.pool2 = e2cnn_nn.SequentialModule(
+            e2cnn_nn.PointwiseAvgPoolAntialiased(
+                out_type, sigma=0.66, stride=2)
+        )
+
         self.gpool = e2cnn_nn.GroupPooling(out_type)
-        
-    def forward(self, input: torch.Tensor):   
+
+    def forward(self, input: torch.Tensor):
         x = e2cnn_nn.GeometricTensor(input, self.input_type)
         x = self.block1(x)
         x = self.pool1(x)
@@ -75,34 +86,38 @@ class Encoder(torch.nn.Module):
         x = self.pool2(x)
         x = self.gpool(x)
         x = x.tensor
-        
-        return x 
-    
+
+        return x
+
+
 class Decoder(nn.Module):
-    
+
     def __init__(self):
         super(Decoder, self).__init__()
 
         self.conv_block1 = nn.Sequential(
-            nn.ConvTranspose2d(16, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16, 64, kernel_size=3, stride=2,
+                               padding=1, output_padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
         )
-        
+
         self.conv_block2 = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2,
+                               padding=1, output_padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
         )
-        
+
         self.conv_block3 = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2,
+                               padding=1, output_padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 16, kernel_size=3, padding=1),
@@ -111,36 +126,39 @@ class Decoder(nn.Module):
         )
 
         self.conv_block4 = nn.Sequential(
-            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=2,
+                               padding=1, output_padding=1),
             nn.BatchNorm2d(3),
             nn.ReLU(inplace=True),
             nn.Conv2d(3, 3, kernel_size=3, padding=1),
             nn.BatchNorm2d(3),
             nn.ReLU(inplace=True),
         )
-        
+
     def forward(self, x):
         x = self.conv_block1(x)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
         x = self.conv_block4(x)
         return x
-    
+
+
 class LinearDecoder(nn.Module):
     def __init__(self):
         super(LinearDecoder, self).__init__()
-        
+
         self.conv_block1 = nn.Sequential(
-            nn.ConvTranspose2d(16, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16, 64, kernel_size=3, stride=2,
+                               padding=1, output_padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
         )
-        
+
         self.flatten = nn.Flatten()
-        
+
         self.fc = nn.Sequential(nn.Linear(32*32*64, 512),
                                 nn.ReLU(inplace=True),
                                 nn.Linear(512, 128),
@@ -150,44 +168,96 @@ class LinearDecoder(nn.Module):
                                 nn.Linear(32, 32),
                                 nn.ReLU(inplace=True),
                                 nn.Linear(32, 5))
-        
+
     def forward(self, x):
         x = self.conv_block1(x)
         x = self.flatten(x)
         x = self.fc(x)
         return x
-                                
-        
-    
-class Autoencoder(nn.Module):
-    
-    def __init__(self):
+
+
+class Autoencoder(pl.LightningModule):
+
+    def __init__(self, config):
         super(Autoencoder, self).__init__()
-        self.encoder = Encoder(N=4, reflections=True)
+        self.encoder = EquivariantEncoder(N=4, reflections=True)
         self.decoder1 = Decoder()
         self.decoder2 = LinearDecoder()
-    
+
+        self.config = config
+        self.val_img_losses = []
+        self.val_param_losses = []
+        self.val_losses = []
+
     def forward(self, x):
         x = self.encoder(x)
         x1 = self.decoder1(x)
         x2 = self.decoder2(x)
         return x1, x2
+
+    def training_step(self, batch, batch_idx):
+        original_image, lensed_image, label, params = batch
+        pred_img, pred_params = self(lensed_image)
+        img_loss = F.mse_loss(pred_img, original_image)
+        param_loss = F.mse_loss(pred_params, params)
+        loss = img_loss + param_loss
+        self.log('train_img_loss', img_loss,on_step=True, on_epoch=True, logger=True)
+        self.log('train_param_loss', param_loss, on_step=True, on_epoch=True, logger=True)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        original_image, lensed_image, label, params = batch
+        pred_img, pred_params = self(lensed_image)
+        img_loss = F.mse_loss(pred_img, original_image)
+        param_loss = F.mse_loss(pred_params, params)
+        loss = img_loss + param_loss
+
+        self.val_img_losses.append(img_loss)
+        self.val_param_losses.append(param_loss)
+        self.val_losses.append(loss)
+
     
-    
-def load_autoencoder():
-    autoencoder = Autoencoder()
+    def on_validation_epoch_end(self):
+        self.log('val_img_loss', torch.stack(self.val_img_losses).mean(), on_step=False, on_epoch=True, logger=True)
+        self.log('val_param_loss', torch.stack(self.val_param_losses).mean(), on_step=False, on_epoch=True, logger=True)
+        self.log('val_loss', torch.stack(self.val_losses).mean(), on_step=False, on_epoch=True, logger=True)
+
+        self.val_img_losses.clear()
+        self.val_param_losses.clear()
+        self.val_losses.clear()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.config['parameters']['lr'],
+                                      weight_decay=self.config['parameters']['weight_decay'])
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.config['parameters']['milestones'],
+                                                            gamma=self.config['parameters']['lr_decay'])
+        return [optimizer], [scheduler]
+
+
+
+
+def load_autoencoder(config):
+    autoencoder = Autoencoder(config)
     return autoencoder
-    
+
+
 if __name__ == '__main__':
     
-    autoencoder = load_autoencoder()
+    parser = argparse.ArgumentParser(description = 'Train the models')
+    parser.add_argument('--config', metavar = 'config', required=True,
+                    help='Location of the config file')
+
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    autoencoder = load_autoencoder(config)
 
     x = torch.rand(size=(1, 3, 256, 256))
     img, vec = autoencoder(x)
     print(img.shape)
     print(vec.shape)
     print(f'Trainable parameters = {sum(p.numel() for p in autoencoder.parameters() if p.requires_grad)}')
-    
-        
-
-
--- src/scripts/train.py
+++ src/scripts/train.py
@@ -11,122 +11,15 @@ from torch import optim
 from torchvision import transforms
 from autoencoder import load_autoencoder
 from dataset import LensingDataset
-from tqdm import tqdm
-import random
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.trainer import Trainer
+from lightning.pytorch import seed_everything
 
-
-def set_all_seeds(num):
-    random.seed(num)
-    np.random.seed(num)
-    torch.manual_seed(num)
-    
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(num)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
-def train_model(model, train_dataloader, val_dataloader, optimizer, scheduler = None, epochs=100, device='cuda', save_dir='checkpoints', early_stopping_patience=10, report_interval=5):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    
-    if device == 'cuda' and torch.cuda.device_count() > 1:
-        model.to(device)
-        model = nn.DataParallel(model)
-        
-    else:
-        model.to(device)
-    
-    print("Model Loaded to Device!")
-    best_val_loss = 0
-    no_improvement_count = 0
-    train_losses, val_losses, steps = [], [], []
-    print("Training Started!")
-    for epoch in range(epochs):
-        model.train()
-        train_loss = 0.0
-        for i, batch in tqdm(enumerate(train_dataloader, 0), unit="batch", total=len(train_dataloader)):
-            original_image, lensed_image, label, params = batch
-            original_image, lensed_image, params = original_image.to(device), lensed_image.to(device), params.to(device)
-            
-            optimizer.zero_grad()
-            img_output, param_output = model(lensed_image)
-            loss = nn.MSELoss()
-            img_loss = loss(img_output, lensed_image)
-            param_loss = loss(param_output, params)
-            sum_loss = img_loss + param_loss
-            sum_loss.backward()
-            optimizer.step()
-
-            train_loss += sum_loss.item()
-            train_losses.append(sum_loss.item())
-            steps.append(epoch * len(train_dataloader) + i + 1)
-
-        train_loss /= len(train_dataloader)
-        print(f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4e}")
-
-        if scheduler is not None:
-            scheduler.step()
-
-        if (epoch + 1) % report_interval == 0:
-            model.eval()
-            val_loss = 0.0
-
-            with torch.no_grad():
-                for batch in val_dataloader:
-                    original_image, lensed_image, label, params = batch
-                    original_image, lensed_image, params = original_image.to(device), lensed_image.to(device), params.to(device)
-                    img_output, param_output = model(lensed_image)
-                    loss = nn.MSELoss()
-                    img_loss = loss(img_output, lensed_image)
-                    param_loss = loss(param_output, params)
-                    loss = img_loss + param_loss
-                    sum_loss = img_loss + param_loss
-                    val_loss += sum_loss.item()
-                    val_losses.append(sum_loss.item())
-
-            val_loss /= len(val_dataloader)
-            lr = scheduler.get_last_lr()[0] if scheduler is not None else optimizer.param_groups[0]['lr']
-            print(f"Epoch: {epoch + 1}, Validation Loss: {val_loss:.4f}, Learning rate: {lr}")
-
-            if val_loss > best_val_loss:
-                best_val_loss= val_loss
-                no_improvement_count = 0
-                best_val_epoch = epoch + 1
-                torch.save(model.module.state_dict(), os.path.join(save_dir, f"best_model.pt"))
-            else:
-                no_improvement_count += 1
-
-            if no_improvement_count >= early_stopping_patience:
-                print(f"Early stopping after {early_stopping_patience} epochs without improvement.")
-                break
-
-    torch.save(model.module.state_dict(), os.path.join(save_dir, "final_model.pt"))
-    
-    # Plot loss vs. training step graph
-    plt.figure(figsize=(10, 5))
-    plt.plot(steps, train_losses)
-    plt.xlabel('Training Steps')
-    plt.ylabel('Loss')
-    plt.title('Train Loss vs. Training Steps')
-    plt.savefig(os.path.join(save_dir, "train_loss_vs_training_steps.png"), bbox_inches='tight')
-    
-    plt.figure(figsize=(10, 5))
-    plt.plot(steps, val_losses)
-    plt.xlabel('Training Steps')
-    plt.ylabel('Validation Loss')
-    plt.title('Validation Loss vs. Training Steps')
-    plt.savefig(os.path.join(save_dir, "val_loss_vs_training_steps.png"), bbox_inches='tight')
-    
-    return best_val_epoch, best_val_loss, train_losses[-1]
 
 
 def main(config):
-    model = load_autoencoder()
-    params_to_optimize = [p for p in model.parameters() if p.requires_grad]
-    optimizer = optim.AdamW(params_to_optimize, lr = config['parameters']['lr'], 
-                            weight_decay = config['parameters']['weight_decay'])
-
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones = config['parameters']['milestones'],gamma=config['parameters']['lr_decay'])
     
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -138,6 +31,9 @@ def main(config):
         transforms.RandomVerticalFlip(p=0.3),
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
+
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    save_dir = config['save_dir'] + 'autoencoder_' + timestr
     
     print("Loading train dataset!")
     start = time.time()
@@ -148,40 +44,55 @@ def main(config):
     val_len = int(config['parameters']['val_size'] * len(train_dataset))
     train_len = len(train_dataset) - val_len
     train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_len, val_len])
-    train_dataloader = DataLoader(train_dataset, batch_size=config['parameters']['batch_size'], shuffle=True, 
-                                  pin_memory = (True if torch.cuda.is_available() else False), num_workers=4)
-    val_dataloader = DataLoader(val_dataset, batch_size=config['parameters']['batch_size'], shuffle=True, 
-                                pin_memory = (True if torch.cuda.is_available() else False), num_workers=4)
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    save_dir = config['save_dir'] + config['model'] + '_' + timestr
-    best_val_epoch, best_val_acc, final_loss = train_model(model, train_dataloader, val_dataloader, optimizer, scheduler, epochs=config['parameters']['epochs'], device=device, save_dir=save_dir,early_stopping_patience=config['parameters']['early_stopping'], report_interval=config['parameters']['report_interval'])
-    print('Training Done')
-    
-    config['best_val_acc'] = best_val_acc
-    config['best_val_epoch'] = best_val_epoch
-    config['final_loss'] = final_loss
-    # config['feature_fields'] = feature_fields
-
-    file = open(f'{save_dir}/config.yaml',"w")
-    yaml.dump(config, file)
-    file.close()
+    train_dataloader = DataLoader(train_dataset, batch_size=config['parameters']['batch_size'], num_workers = int(os.environ['SLURM_CPUS_PER_TASK']), pin_memory=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=config['parameters']['batch_size'], num_workers = int(os.environ['SLURM_CPUS_PER_TASK']), pin_memory=True)
 
     
+    model = load_autoencoder(config=config)
+
+    wand_logger = WandbLogger(project='DE-2-Lensing')
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=os.path.join(save_dir, 'checkpoints'),
+        verbose=True,
+        save_top_k=2,
+        monitor="val_loss",
+        mode="min",
+        save_on_train_epoch_end=True,
+        auto_insert_metric_name=True,
+        filename='autoencoder-{epoch:02d}-{val_loss:.2f}'
+    )
+
+    early_stop_callback = EarlyStopping(
+        monitor='val_loss',
+        mode='min',
+        patience=config['parameters']['early_stopping'],
+        verbose=True
+    )
+
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    trainer = Trainer(accelerator='auto',max_epochs=config['parameters']['epochs'],callbacks=[checkpoint_callback, early_stop_callback, lr_monitor], logger=wand_logger, deterministic='warn')
+
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+    
+
+
+
+
+
 if __name__ == '__main__':
-
-    device = ('cuda' if torch.cuda.is_available() else 'cpu')
-        
-    set_all_seeds(42)
-
+    
     parser = argparse.ArgumentParser(description = 'Train the models')
     parser.add_argument('--config', metavar = 'config', required=True,
                     help='Location of the config file')
 
     args = parser.parse_args()
 
-
     with open(args.config, 'r') as f:
         config = yaml.safe_load(f)
-   
+    seed_everything(42, workers=True)
     main(config)
+    
