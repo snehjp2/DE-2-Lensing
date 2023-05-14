f178f793711a94c65e28749951c34262544ef497
purvikpatel <purvikpatel619@gmail.com>
2023-05-13 23:05:24 -0400

    Data loaded from h5 file in init

--- src/scripts/dataset.py
+++ src/scripts/dataset.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 import torchvision.transforms as T
 import matplotlib.pyplot as plt
 from torchvision import transforms
+import time
 
 class LensingDataset(Dataset):
     """Loading Galaxy10 DECals test dataset from .h5 file.
@@ -16,20 +17,24 @@ class LensingDataset(Dataset):
     """
     def __init__(self,dataset_path : str, transform = None) :
         self.dataset_path = dataset_path
-        self.dataset = None
+        #self.dataset = None
         self.transform = transform
         with h5py.File(self.dataset_path,"r") as f:
+            self.original_images = f['original_images'][()]
+            self.lensed_images = f['lensed_images'][()]
+            self.labels = f['labels'][()]
+            self.lens_params = f['lens_params_list'][()]
             self.length = len(f['labels'][()])
 
     def __getitem__(self, idx):
 
-        if self.dataset is None:
-            self.dataset = h5py.File(self.dataset_path,"r")
+        #if self.dataset is None:
+        #    self.dataset = h5py.File(self.dataset_path,"r")
 
-        original_image = self.dataset['original_images'][idx]
-        lensed_image = self.dataset['lensed_images'][idx]
-        label = torch.tensor(self.dataset['labels'][idx],dtype=torch.long)
-        params = torch.tensor(self.dataset['lens_params_list'][idx], dtype=torch.float)
+        original_image = self.original_images[idx]
+        lensed_image = self.lensed_images[idx]
+        label = torch.tensor(self.labels[idx],dtype=torch.long)
+        params = torch.tensor(self.lens_params[idx], dtype=torch.float)
         
         if self.transform:
             original_image = self.transform(original_image)
@@ -42,8 +47,12 @@ class LensingDataset(Dataset):
 if __name__ == '__main__':
 
     transform = transforms.ToTensor()
-    PATH = '/Users/snehpandya/Projects/DE-2-Lensing/data/lensing_dataset_SIS.h5'
+    PATH = '/scratch/pandya.sne/E2_Lensing/lensing_dataset_SIS.h5'
+    print("Loading dataset...")
+    start = time.time()
     dataset = LensingDataset(PATH,transform=transform)
+    end = time.time()
+    print(f"Dataset loaded in {end-start} seconds")
     img, lensed_img, label, params = dataset[12342]
     print(img.shape)
     print(lensed_img.shape)
